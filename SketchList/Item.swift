f73894d2d473e40492fa78dccb7ad3f9402f830d
Sam Blackwood <sam.blackwood23@gmail.com>
2026-07-01 21:29:32 -0600

    Initial Commit

---
+++ SketchList/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SketchList
+//
+//  Created by Sam Blackwood on 7/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SketchListTests/SketchListTests.swift
@@ -0,0 +1,18 @@
+//
+//  SketchListTests.swift
+//  SketchListTests
+//
+//  Created by Sam Blackwood on 7/1/26.
+//
+
+import Testing
+
+struct SketchListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
